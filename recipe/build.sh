9ce14644e297f32736a6eb8b33658080c7930a82
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2020-01-06 16:30:57 -0500

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,7 +6,10 @@ cd build
 if [ ${float_prec} == "high" ]; then
     export PREC_DEF="-DHIGH_PREC"
 fi
-cmake -D PKG_USER-DEEPMD=ON -D PKG_USER-MISC=ON -D FFT=FFTW3 -D CMAKE_INSTALL_PREFIX=${PREFIX} -D CMAKE_CXX_FLAGS="${PREC_DEF} -I${PREFIX}/include -I${PREFIX}/include/deepmd -L${PREFIX}/lib -Wl,--no-as-needed -lrt -ldeepmd_op -ldeepmd -ltensorflow_cc -ltensorflow_framework -Wl,-rpath=${PREFIX}/lib" ../cmake
+if [ ${dp_variant} == "gpu" ]; then
+    export DEEPMD_CUDA_LINK="-ldeepmd_op_cuda"
+fi
+cmake -D PKG_USER-DEEPMD=ON -D PKG_USER-MISC=ON -D FFT=FFTW3 -D CMAKE_INSTALL_PREFIX=${PREFIX} -D CMAKE_CXX_FLAGS="${PREC_DEF} -I${PREFIX}/include -I${PREFIX}/include/deepmd -L${PREFIX}/lib -Wl,--no-as-needed -lrt -ldeepmd_op ${DEEPMD_CUDA_LINK} -ldeepmd -ltensorflow_cc -ltensorflow_framework -Wl,-rpath=${PREFIX}/lib" ../cmake
 make -j${NUM_CPUS}
 make install
 cd ..
