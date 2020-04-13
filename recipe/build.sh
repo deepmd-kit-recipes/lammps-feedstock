1571e66eee7896b0339ef880cad37c314af02d81
zhangyongsdu <zhangyongsdu@gmail.com>
2020-04-13 14:09:20 +1000

    include mc package in lammps

--- recipe/build.sh
+++ recipe/build.sh
@@ -9,7 +9,7 @@ fi
 if [ ${dp_variant} == "gpu" ]; then
     export DEEPMD_CUDA_LINK="-ldeepmd_op_cuda"
 fi
-cmake -D PKG_USER-DEEPMD=ON -D PKG_USER-MISC=ON -D FFT=FFTW3 -D CMAKE_INSTALL_PREFIX=${PREFIX} -D CMAKE_CXX_FLAGS="${PREC_DEF} -I${PREFIX}/include -I${PREFIX}/include/deepmd -L${PREFIX}/lib -Wl,--no-as-needed -lrt -ldeepmd_op ${DEEPMD_CUDA_LINK} -ldeepmd -ltensorflow_cc -ltensorflow_framework -Wl,-rpath=${PREFIX}/lib" ../cmake
+cmake -D PKG_USER-DEEPMD=ON -D PKG_MC=ON -D PKG_USER-MISC=ON -D FFT=FFTW3 -D CMAKE_INSTALL_PREFIX=${PREFIX} -D CMAKE_CXX_FLAGS="${PREC_DEF} -I${PREFIX}/include -I${PREFIX}/include/deepmd -L${PREFIX}/lib -Wl,--no-as-needed -lrt -ldeepmd_op ${DEEPMD_CUDA_LINK} -ldeepmd -ltensorflow_cc -ltensorflow_framework -Wl,-rpath=${PREFIX}/lib" ../cmake
 make -j${NUM_CPUS}
 make install
 cd ..
