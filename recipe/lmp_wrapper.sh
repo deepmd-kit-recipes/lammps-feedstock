d37749821a015c0c33a955e7f94ceed20f4e9c2f
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2022-09-13 19:06:24 -0400

    add wrapper to throw warning

---
+++ recipe/lmp_wrapper.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+if [ "$PREFIX" != "$CONDA_PREFIX" ]
+then
+   cat << EOF
+Warning:
+This LAMMPS executable is in a conda environment, but the environment has
+not been activated. Libraries may fail to load. To activate this environment
+please see https://conda.io/activation.
+EOF
+fi
+$PREFIX/bin/lmp "$@"
+
